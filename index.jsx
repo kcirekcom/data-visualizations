3cef495e7dfa701db4de5e1160a0ff2bb3a2ecf1
kcirekcom <erick.f.mock@gmail.com>
2017-02-26 22:19:54 -0800

    added sample json///create react chart component

---
+++ index.jsx
@@ -0,0 +1,48 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+var Chart = require('react-d3-core').Chart;
+var LineChart = require('react-d3-basic').LineChart;
+
+(function() {
+
+  var chartData = require('json!../data/pitching-stats.json');
+
+  var width = 700,
+    height = 300,
+    margins = {left: 100, right: 100, top: 50, bottom: 50},
+    title = 'User sample',
+    chartSeries = [
+      {
+        field: 'BMI',
+        name: 'BMI',
+        color: '#ff7f0e'
+      }
+    ],
+    x = function(d) {
+      return d.index;
+    };
+
+  ReactDOM.render(
+    <Chart
+      title={title}
+      width={width}
+      height={height}
+      margins= {margins}
+      >
+      <LineChart
+        showXGrid= {false}
+        showYGrid= {false}
+        margins= {margins}
+        title={title}
+        data={chartData}
+        width={width}
+        height={height}
+        chartSeries={chartSeries}
+        x={x}
+      />
+    </Chart>
+  , document.getElementById('line-user')
+  );
+})();
