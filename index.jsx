9ad897ec524390066ed8dd86a42c116c03a0f178
kcirekcom <erick.f.mock@gmail.com>
2017-03-05 15:24:45 -0800

    commented out previous code

--- index.jsx
+++ index.jsx
@@ -1,41 +1,42 @@
 'use strict';
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-var LineTooltip = require('react-d3-tooltip').LineTooltip;
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-(function() {
+// var LineTooltip = require('react-d3-tooltip').LineTooltip;
 
-  var chartData = require('./data/pitching-stats.json');
-
-  var width = 900,
-    height = 300,
-    margins = {left: 100, right: 100, top: 50, bottom: 50},
-    title = 'Wins',
-    chartSeries = [
-      {
-        field: 'wins',
-        name: 'wins',
-        color: '#ff7f0e'
-      }
-    ],
-    x = function(d) {
-      return d.rank;
-    };
-
-  ReactDOM.render(
-    <LineTooltip
-      showXGrid={false}
-      showYGrid={false}
-      margins={margins}
-      title={title}
-      data={chartData}
-      width={width}
-      height={height}
-      chartSeries={chartSeries}
-      x={x}
-      xScale='ordinal'
-    />
-  , document.getElementById('line-user')
-  );
-})();
+// (function() {
+//
+//   var chartData = require('./data/pitching-stats.json');
+//
+//   var width = 900,
+//     height = 300,
+//     margins = {left: 100, right: 100, top: 50, bottom: 50},
+//     title = 'Wins',
+//     chartSeries = [
+//       {
+//         field: 'wins',
+//         name: 'wins',
+//         color: '#ff7f0e'
+//       }
+//     ],
+//     x = function(d) {
+//       return d.rank;
+//     };
+//
+//   ReactDOM.render(
+//     <LineTooltip
+//       showXGrid={false}
+//       showYGrid={false}
+//       margins={margins}
+//       title={title}
+//       data={chartData}
+//       width={width}
+//       height={height}
+//       chartSeries={chartSeries}
+//       x={x}
+//       xScale='ordinal'
+//     />
+//   , document.getElementById('line-user')
+//   );
+// })();
